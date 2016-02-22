6d3d71dabf1bb5a42ed50a2f5375ee1a51147820
Benny van Zuiden <zuiden@lorentz.leidenuniv.nl>
2016-02-22 11:47:19 +0100

    first commit

---
+++ hdf5vscsv.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import string
+import random
+import pandas as pd
+import numpy as np
+
+matrix=np.random.random((3000,10))
+my_cols=[random.choice(string.ascii_uppercase) for x in range(matrix.shape[1])]
+mydf=pd.DataFrame(matrix)
+mydf['test']='test'
+store=pd.HDFStore('test.h5',complevel=9,complib='bzip2')
+store['test']=mydf
+store.close()
+mydf.to_csv('test.csv',sep=';')
